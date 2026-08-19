dd8e44f32b83c31767c56832b6ece5ff010f1a71
viniciusaraujoop <araujovinicius249@gmail.com>
2026-08-18 22:34:51 -0300

    feat(crm): connect repurchase opportunities to CRM

--- app/painel/crm/page.tsx
+++ app/painel/crm/page.tsx
@@ -1,5 +1,11 @@
 import CrmWorkspaceV2 from '@/components/crm/CrmWorkspaceV2'
+import RepurchaseOpportunitiesPanel from '@/components/painel/RepurchaseOpportunitiesPanel'
 
 export default function CrmPage() {
-  return <CrmWorkspaceV2 />
+  return (
+    <div className="grid gap-4">
+      <CrmWorkspaceV2 />
+      <RepurchaseOpportunitiesPanel compact />
+    </div>
+  )
 }
